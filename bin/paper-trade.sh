8bb707e82605cc9c204adc41c78be4806ab57311
Rick Tonoli <rick@tonoli.co.nz>
2018-01-06 23:48:09 +1300

    Dumbass, using wrong files for config

--- bin/paper-trade.sh
+++ bin/paper-trade.sh
@@ -8,8 +8,8 @@ cd ~/zenbot
 
 if [[ -f "strategies/cexio.$1-USD/$3/$2_data.json" ]]; then
  echo "Staring paper trading"
- echo "Executing: zenbot trade --paper cexio.$1-USD --conf strategies/cexio.$1-USD/$3/$2_data.json --currency_capital 1000 --buy_pct 100 --sell_pct 100"
- /usr/bin/screen -S Paper$1 -d -m bash -c "zenbot trade --paper cexio.$1-USD --conf strategies/cexio.$1-USD/$3/$2_data.json --currency_capital 1000 --buy_pct 100 --sell_pct 100"
+ echo "Executing: zenbot trade --paper cexio.$1-USD --conf strategies/cexio.$1-USD/$3/$2.conf --currency_capital 1000 --buy_pct 100 --sell_pct 100"
+ /usr/bin/screen -S Paper$1 -d -m bash -c "zenbot trade --paper cexio.$1-USD --conf strategies/cexio.$1-USD/$3/$2.conf --currency_capital 1000 --buy_pct 100 --sell_pct 100"
 # /usr/bin/screen -r Paper$1
 else 
   echo "No config file found at strategies/cexio.$1-USD/$3/$2_data.json"
